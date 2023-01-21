102fbf456849c5701efd87177c7f416c4201900e
dong-yeol.lee <dong-yeol.lee@linecorp.com>
2023-01-21 18:20:25 +0900

    Integrate with spring container

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -1,17 +1,9 @@
 package com.tobyspring.helloboot;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import com.tobyspring.helloboot.controller.HelloController;
 import com.tobyspring.helloboot.service.SimpleHelloService;
@@ -19,33 +11,21 @@ import com.tobyspring.helloboot.service.SimpleHelloService;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = webServerFactory.getWebServer(servletContext -> servletContext.addServlet("hello",
+                                                                                                                new DispatcherServlet(this))
+                                                                                                    .addMapping("/*"));
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
-        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp)
-                    throws IOException {
-                    System.out.println("<<<<< Servlet >>>>>");
-                    // 공통 기능 구현 (인증, 다국어, 로깅 등등)
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().print(ret);
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
-        });
-        webServer.start();
     }
 }
--- src/main/java/com/tobyspring/helloboot/controller/HelloController.java
+++ src/main/java/com/tobyspring/helloboot/controller/HelloController.java
@@ -2,15 +2,21 @@ package com.tobyspring.helloboot.controller;
 
 import java.util.Objects;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.tobyspring.helloboot.service.HelloService;
 
 //@RestController
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {this.helloService = helloService;}
 
-    //    @GetMapping("/hello")
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         System.out.println("<<<<< Controller >>>>>");
         return helloService.sayHello(Objects.requireNonNull(name));

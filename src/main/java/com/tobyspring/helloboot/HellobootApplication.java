04ef73e54f5b97b0e741b2def6988aedf18733e6
dong-yeol.lee <dong-yeol.lee@linecorp.com>
2023-01-24 20:02:22 +0900

    Add tomcat and servlet bean

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -2,6 +2,8 @@ package com.tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -9,16 +11,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HellobootApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
 
-                TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = webServerFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
-                                                                                                                new DispatcherServlet(this))
-                                                                                                    .addMapping("/*"));
+                final ServletWebServerFactory webServerFactory = getBean(ServletWebServerFactory.class);
+                final DispatcherServlet dispatcherServlet = getBean(DispatcherServlet.class);
+
+                final WebServer webServer = webServerFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
+                                                                                                                      dispatcherServlet)
+                                                                                                          .addMapping("/*"));
                 webServer.start();
             }
         };

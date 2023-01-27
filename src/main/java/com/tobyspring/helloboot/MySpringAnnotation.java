0d615bdb9abf563349270e9ef8c6246ab716da81
dong-yeol.lee <dong-yeol.lee@linecorp.com>
2023-01-27 16:01:45 +0900

    Add mySpringAnnotation

---
+++ src/main/java/com/tobyspring/helloboot/MySpringAnnotation.java
@@ -0,0 +1,16 @@
+package com.tobyspring.helloboot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ComponentScan
+@Configuration
+public @interface MySpringAnnotation {
+}
